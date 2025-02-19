c97dfbf4769aa07b76c45e42b2bb7f4d2cf75f8f
Danyaal-ADA <Danyaal.Mohammad@ada.ac.uk>
2025-02-19 16:40:39 +0000

    finished personal trainingf and staff management queries

--- src/personal_training.sql
+++ src/personal_training.sql
@@ -8,3 +8,13 @@
 
 -- 1. List all personal training sessions for a specific trainer
 -- TODO: Write a query to list all personal training sessions for a specific trainer
+ 
+SELECT ps.session_id,m.first_name || ' ' || m.last_name AS member_name,ps.session_date, ps.start_time,
+       ps.end_time
+FROM personal_training_sessions ps
+JOIN members m
+ON ps.member_id = m.member_id
+JOIN staff st
+ON ps.staff_id = st.staff_id
+WHERE st.first_name = 'Ivy' AND st.last_name = 'Irwin'
+ORDER BY ps.session_date, ps.start_time;
